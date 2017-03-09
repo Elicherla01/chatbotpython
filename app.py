4d38f68505a4bc3e039d3882dfdf53c2e19ad9c0
Ravindra Prasad <ravindraprasad@yahoo.com>
2017-03-09 11:24:36 +0530

    initial

--- app.py
+++ app.py
@@ -8,25 +8,6 @@ from fbmq import Page
 
 app = Flask(__name__)
 
-page = fbmq.Page(PAGE_ACCESS_TOKEN)
-
-@app.route('/webhook', methods=['POST'])
-def webhook():
-  page.handle_webhook(request.get_data(as_text=True))
-  return "ok"
-@page.handle_message
-def message_handler(event):
-  """:type event: fbmq.Event"""
-  sender_id = event.sender_id
-  message = event.message_text
-
-  page.send(sender_id, "thank you! your message is '%s'" % message)
-
-@page.after_send
-def after_send(payload, response):
-  """:type payload: fbmq.Payload"""
-  print("complete")
-
 
 @app.route('/', methods=['GET'])
 def verify():
@@ -61,15 +42,15 @@ def webhook():
 
                     if message_text == "Hi":
                         send_message(sender_id, "Glad to see you here. How can i help you?")
-                    
+
                     if message_text == "Hello":
                         send_message(sender_id, "Amazing you said Hello")
-                    
+
                     if message_text == "Bye":
                         send_message(sender_id, "Sorry to see you going. Have fun")
-                    
-                    
-                    
+
+
+
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
 
