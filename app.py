5a4309d40788c20484717d99850d4a892dda4b4c
Ravindra Prasad <ravindraprasad@yahoo.com>
2017-03-09 11:05:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,28 @@ import json
 
 import requests
 from flask import Flask, request
+from fbmq import Page
 
 app = Flask(__name__)
+
+page = fbmq.Page(PAGE_ACCESS_TOKEN)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+  page.handle_webhook(request.get_data(as_text=True))
+  return "ok"
+@page.handle_message
+def message_handler(event):
+  """:type event: fbmq.Event"""
+  sender_id = event.sender_id
+  message = event.message_text
+
+  page.send(sender_id, "thank you! your message is '%s'" % message)
+
+@page.after_send
+def after_send(payload, response):
+  """:type payload: fbmq.Payload"""
+  print("complete")
 
 
 @app.route('/', methods=['GET'])
